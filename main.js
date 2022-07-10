c5c218ab2f937a50f8d3d626f30beed299510bfe
Salanki "Lockgar" Laszlo <hgtsoul@gmail.com>
2022-07-10 22:28:17 +0200

    Update main.js

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ const accept = '✅'
 const prefix = '*'
 const allIntents = new Intents(32767);
 const client = new Client({ intents: allIntents })
+client.login(token);
 const token = process.env.token
 let stream = new Stream(token)
 const url_expression = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi;
